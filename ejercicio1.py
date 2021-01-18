93099229a93d1e1a968c94d2881cc3380f56537f
Jaime Salcedo <jaimesalcedo7.22@hotmail.com>
2021-01-18 17:52:37 -0500

    Datos

--- ejercicio1.py
+++ ejercicio1.py
@@ -8,5 +8,17 @@ This is a temporary script file.
 import numpy as np
 import flopy
 
-git init
+modelname = "model1"
+mf = flopy.modflow.Modflow(modelname, exe_name="mf2005")
 
+Lx = 1000.0
+Ly = 1000.0
+ztop = 0.0
+zbot = -50.0
+nlay = 1
+nrow = 10
+ncol = 10
+delr = Lx / ncol
+delc = Ly / nrow
+delv = (ztop - zbot) / nlay
+botm = np.linspace(ztop, zbot, nlay + 1)
